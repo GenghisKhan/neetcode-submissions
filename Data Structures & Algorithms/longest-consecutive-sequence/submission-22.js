6d0bd15fab678f1cb33a2af42b9c23f32f4246c4
Trung Lam <dajger@gmail.com>
2026-07-19 11:45:19 -0700

    Add: longest-consecutive-sequence - submission-22

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-22.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if (!nums?.length) return 0;
+        const seq = [];
+        const numsSet = new Set(nums);
+        let index = 0;
+        for (const num of numsSet) {
+            const startSeq = !numsSet.has(num - 1);
+            if (startSeq) {
+                let count = seq[num] || 1;
+                let currNum = num;
+                do {
+                    if (currNum > num && index + 1 <= numsSet.size) {
+                        count++;
+                    }
+                    currNum = currNum + 1;
+                } while (numsSet.has(currNum));
+                seq.push(count);
+            }
+            index++;
+        }
+        return Math.max(...seq) || 0;
+    }
+}
