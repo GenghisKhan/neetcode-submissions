dc866bc5851f7ba309aae874e8f8c16bcb049ccb
Trung Lam <dajger@gmail.com>
2026-07-20 17:47:02 -0700

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b);
+        const result = [];
+        const length = nums.length;
+        for (let i = 0; i < length; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+            let leftP = i + 1;
+            let rightP = length - 1;
+            const curVal = nums[i];
+            while (leftP < rightP) {
+                const leftVal = nums[leftP];
+                const rightVal = nums[rightP];
+                const sum = curVal + leftVal + rightVal;
+                if (sum > 0) {
+                    rightP--;
+                } else if (sum < 0) {
+                    leftP++;
+                } else {
+                    result.push([curVal, leftVal, rightVal]);
+                    while (leftP < rightP && nums[leftP] === nums[leftP + 1]) leftP++;
+                    while (leftP < rightP && nums[rightP] === nums[rightP - 1]) rightP--;
+                    leftP++;
+                    rightP--;
+                }
+            }
+        }
+        return result;
+    }
+}
